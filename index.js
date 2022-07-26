9922036b1bdd9b3da2b93833584308b7e47a5358
Andrej Vyšný <andrej.vysny@websupport.sk>
2022-07-27 01:12:52 +0200

    github actions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World from docker swarm 1'));
+app.get('/', (req, res) => res.send('Hello World from docker swarm 2 <br/><h1>Some change</h1>'));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
