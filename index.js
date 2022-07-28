79fb22c7499d514e03f5fb2da47c2eb8146527bf
Andrej Vyšný <andrej.vysny@websupport.sk>
2022-07-28 19:24:49 +0200

    image

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send("version 4"));
+app.get('/', (req, res) => res.send("version 5"));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
